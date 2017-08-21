a7eda8bd55b715eedfb13ee89b41f6b66bc61681
Jane Lee <yesdoing13@gmail.com>
2017-08-21 16:36:35 +0900

    Pawn 색에 따른 출력 문자 부여

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -3,20 +3,32 @@ package pieces;
 public class Pawn {
 	public static final String PIECE_BLACK = "black";
 	public static final String PIECE_WHITE = "white";
+	public static final char PIECE_WHITE_REPRESENTATION = 'p';
+	public static final char PIECE_BLACK_REPRESENTATION = 'P';
 	
 	String color;
+	char representation;
 	
 	public Pawn() {
-		this.color = "white";
+		this.color = PIECE_WHITE;
+		this.representation = PIECE_WHITE_REPRESENTATION;
 	}
 	
 	public Pawn(String color) {
 		this.color = color;
 	}
 
+	public Pawn(String color, final char representation) {
+		this.color = color;
+		this.representation = representation;
+	}
 
 	public String getColor() {
 		return color;
+	}
+
+	public char getRepresentation() {
+		return representation;
 	}
 	
 	
--- test/pieces/PawnTest.java
+++ test/pieces/PawnTest.java
@@ -14,20 +14,22 @@ public class PawnTest {
 		
 	}
 	
-	@Test
-	public void 폰_색깔_검사() {
-		verifyPawn("white");
-		verifyPawn("black");
-	}
-
-	private void verifyPawn(String color) {
-		Pawn pawn = new Pawn(color);
+	private void verifyPawn(String color, final char representation) {
+		Pawn pawn = new Pawn(color, representation);
 		assertEquals(color, pawn.getColor());
+		assertEquals(representation, pawn.getRepresentation());
 	}
 
 	@Test
 	public void create_기본생성자() throws Exception {
 		Pawn pawn = new Pawn();
-		assertEquals("white", pawn.getColor());
+		assertEquals(Pawn.PIECE_WHITE, pawn.getColor());
+		assertEquals(Pawn.PIECE_WHITE_REPRESENTATION, pawn.getRepresentation());
+	}
+	
+	@Test
+	public void create() throws Exception {
+		verifyPawn(Pawn.PIECE_WHITE, Pawn.PIECE_WHITE_REPRESENTATION);
+		verifyPawn(Pawn.PIECE_BLACK, Pawn.PIECE_BLACK_REPRESENTATION);
 	}
 }
