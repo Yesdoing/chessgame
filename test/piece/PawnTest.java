12d59d38744ed81662975737f07816661e40e87f
Jane Lee <yesdoing13@gmail.com>
2017-08-21 12:45:52 +0900

    폰 테스트 코드 리펙토링

--- test/piece/PawnTest.java
+++ test/piece/PawnTest.java
@@ -6,20 +6,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PawnTest {
-	
-	Pawn white_pawn;
-	Pawn black_pawn;
-	
+		
 	@Before
 	public void setup() {
-		white_pawn = new Pawn("white");
-		black_pawn = new Pawn("black");
+		
 	}
 	
 	@Test
 	public void 폰_색깔_검사() {
-		assertEquals("white", white_pawn.getColor());
-		assertEquals("black", black_pawn.getColor());
+		verifyPawn("white");
+		verifyPawn("black");
 	}
 
+	private void verifyPawn(String color) {
+		Pawn pawn = new Pawn(color);
+		assertEquals(color, pawn.getColor());
+	}
+
+	
 }
