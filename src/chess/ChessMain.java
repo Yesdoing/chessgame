89ee49f04900353acad37bbfa47a225fb853823f
Jane Lee <yesdoing13@gmail.com>
2017-08-22 16:05:38 +0900

    ChessMain 클래스 생성 while문을 통한 게임 구동

---
+++ src/chess/ChessMain.java
@@ -0,0 +1,28 @@
+package chess;
+
+import java.util.Scanner;
+
+public class ChessMain {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("체스 게임을 시작합니다. ");
+		System.out.println("시작은 start, 종료는 end 명령을 입력하세요. ");
+		
+		while(true) {
+			String command = scan.nextLine();
+			if(command.equals("start")) {
+				Board board = new Board();
+				board.initialize();
+				board.print();
+			} else if(command.equals("end")) {
+				break;
+			} else {
+				System.out.println(command + "는 지원하지 않는 명령어 입니다. ");
+			}
+		}
+		
+		scan.close();
+		
+	}
+	
+}
