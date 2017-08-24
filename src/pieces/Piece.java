ebb976119db3531ed93bdfb0e4ec65fa4097b6f2
Jane Lee <yesdoing13@gmail.com>
2017-08-24 19:27:13 +0900

    코드 리펙토링 - REPRESENTATION 상수들 Enum으로 변경, 테스트 통과

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,95 +1,114 @@
 package pieces;
 
+
 public class Piece {
-	public static final String BLACK_COLOR = "black";
-	public static final String WHITE_COLOR = "white";
-	public static final char WHITE_PAWN_REPRESENTATION = 'p';
-	public static final char BLACK_PAWN_REPRESENTATION = 'P';
-	public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
-	public static final char BLACK_KNIGHT_REPRESENTATION = 'N';	
-	public static final char WHITE_ROOK_REPRESENTATION = 'r';
-	public static final char BLACK_ROOK_REPRESENTATION = 'R';
-	public static final char WHITE_BISHOP_REPRESENTATION = 'b';
-	public static final char BLACK_BISHOP_REPRESENTATION = 'B';
-	public static final char WHITE_QUEEN_REPRESENTATION = 'q';
-	public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
-	public static final char WHITE_KING_REPRESENTATION = 'k';
-	public static final char BLACK_KING_REPRESENTATION = 'K';
+	public enum Color {
+		WHITE, BLACK, NOCOLOR;
+	}
 	
-	String color;
-	char representation;
+	public enum Type {
+		PAWN('p'), KNIGHT('n'), ROOK('r'), BISHOP('b'), QUEEN('q'), KING('k'), NO_PIECE('x');
+		
+		private char representation;
+		
+		Type (char representation) {
+			this.representation = representation;
+		}
+
+		public char getWhiteRepresentation() {
+			return representation;
+		}
+
+		public char getBlackRepresentation() {
+			return Character.toUpperCase(representation);
+		}
+	}
+		
+	Color color;
+	Type type;
 	
 	public boolean isBlack() {
-		return this.color.equals("black");
+		return this.color.equals(Color.BLACK);
 	}
 	
 	public boolean isWhite() {
-		return this.color.equals("white");
+		return this.color.equals(Color.WHITE);
 	}
 	
 	
 	
-	private Piece(String color, char representation) {
+	private Piece(Color color, Type representation) {
 		this.color = color;
-		this.representation = representation;
+		this.type = representation;
 		
 	}
 	
+	private static Piece createWhite(Type type) {
+		return new Piece(Color.WHITE, type);
+	}
+	
+	private static Piece createBlack(Type type) {
+		return new Piece(Color.BLACK, type);
+	}
+	
 	public static Piece createWhitePawn() {
-		return new Piece(WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
+		return createWhite(Type.PAWN);
 	}
 
 	public static Piece createWhiteKing() {
-		return new Piece(WHITE_COLOR, WHITE_KING_REPRESENTATION);
+		return createWhite(Type.KING);
 	}
 
 	public static Piece createWhiteRook() {
-		return new Piece(WHITE_COLOR, WHITE_ROOK_REPRESENTATION);
+		return createWhite(Type.ROOK);
 	}
 
 	public static Piece createWhiteQueen() {
-		return new Piece(WHITE_COLOR, WHITE_QUEEN_REPRESENTATION);
+		return createWhite(Type.QUEEN);
 	}
 	
 	public static Piece createWhiteKnight() {
-		return new Piece(WHITE_COLOR, WHITE_KNIGHT_REPRESENTATION);
+		return createWhite(Type.KNIGHT);
 	}
 
 	public static Piece createWhiteBishop() {
-		return new Piece(WHITE_COLOR, WHITE_BISHOP_REPRESENTATION);
+		return createWhite(Type.BISHOP);
 	}
 	
 	public static Piece createBlackPawn() {
-		return new Piece(BLACK_COLOR, BLACK_PAWN_REPRESENTATION);
+		return createBlack(Type.PAWN);
 	}
 
 	public static Piece createBlackBishop() {
-		return new Piece(BLACK_COLOR, BLACK_BISHOP_REPRESENTATION);
+		return createBlack(Type.BISHOP);
 	}
 	
 	public static Piece createBlackRook() {
-		return new Piece(BLACK_COLOR, BLACK_ROOK_REPRESENTATION);
+		return createBlack(Type.ROOK);
 	}
 	
 	public static Piece createBlackKing() {
-		return new Piece(BLACK_COLOR, BLACK_KING_REPRESENTATION);
+		return createBlack(Type.KING);
 	}
 	public static Piece createBlackQueen() {
-		return new Piece(BLACK_COLOR, BLACK_QUEEN_REPRESENTATION);
+		return createBlack(Type.QUEEN);
 	}
 
 	public static Piece createBlackKnight() {
-		return new Piece(BLACK_COLOR, BLACK_KNIGHT_REPRESENTATION);
+		return createBlack(Type.KNIGHT);
 	}
 	
 
-	public String getColor() {
+	public Color getColor() {
 		return color;
 	}
 
-	public char getRepresentation() {
-		return representation;
+
+	public Type getType() {
+		return type;
 	}
-	
-	
+
+	public static Piece createBlank() {
+		return new Piece(Color.NOCOLOR, Type.NO_PIECE);
+	}
 }
--- test/pieces/PieceTest.java
+++ test/pieces/PieceTest.java
@@ -6,31 +6,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 import pieces.Piece;
+import pieces.Piece.Type;
 
 public class PieceTest {
-		
-	@Before
-	public void setup() {
-		
-	}
 	
-	private void verifyPawn(final Piece piece, final String color, final char representation) {
-		assertEquals(color, piece.getColor());
-		assertEquals(representation, piece.getRepresentation());
-	}
- 
-	@Test
-	public void create_piece() throws Exception {
-		verifyPawn(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
-		verifyPawn(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
-	}
-	
-	@Test
-	public void isWhiteAndBlack() throws Exception {
-		assertTrue(Piece.createWhiteKing().isWhite());
-		assertTrue(Piece.createBlackKing().isBlack());
-	}
+	   @Test
+	    public void create_piece() {
+	        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Type.PAWN);
+	        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Type.KNIGHT);
+	        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Type.ROOK);
+	        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Type.BISHOP);
+	        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Type.QUEEN);
+	        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Type.KING);
 
+	        Piece blank = Piece.createBlank();
+	        assertFalse(blank.isWhite());
+	        assertFalse(blank.isBlack());
+	        assertEquals(Type.NO_PIECE, blank.getType());
+	    }
 
+	    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Type type) {
+	        assertTrue(whitePiece.isWhite());
+	        assertEquals(type, whitePiece.getType());
+
+	        assertTrue(blackPiece.isBlack());
+	        assertEquals(type, blackPiece.getType());
+	    }
 	
 }
